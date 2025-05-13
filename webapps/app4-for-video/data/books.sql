d4c0db8e528e9fb4019cfb09ed964b830a17f3b8
Jeff Ondich <jeff.ondich@gmail.com>
2025-05-13 16:05:55 -0500

    updated books dataset for the app for the video

--- webapps/app4-for-video/data/books.sql
+++ webapps/app4-for-video/data/books.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.1
--- Dumped by pg_dump version 13.1
+-- Dumped from database version 14.11 (Homebrew)
+-- Dumped by pg_dump version 14.17 (Homebrew)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -16,6 +16,9 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+DROP TABLE public.books_authors;
+DROP TABLE public.books;
+DROP TABLE public.authors;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -62,19 +65,20 @@ COPY public.authors (id, surname, given_name, birth_year, death_year) FROM stdin
 0	Willis	Connie	1945	\N
 1	Christie	Agatha	1890	1976
 2	Morrison	Toni	1931	\N
-4	Austen	Jane	1775	1817
-5	Gaiman	Neil	1960	\N
-6	Pratchett	Terry	1948	2015
+3	Conger	Kate	1989	\N
+4	Mac	Ryan	\N	\N
+5	Lewis	Sinclair	1885	1951
+6	Austen	Jane	1775	1817
 7	Brontë	Charlotte	1816	1855
-8	Wodehouse	Pelham Grenville	1881	1975
+8	Wodehouse	P.G.	1881	1975
 9	Márquez	Gabriel García	1927	2014
-10	Lewis	Sinclair	1885	1951
-11	Rushdie	Salman	1947	\N
-12	Bujold	Lois McMaster	1949	\N
-13	Melville	Herman	1819	1891
-14	Brontë	Ann	1820	1849
-15	Brontë	Emily	1818	1848
-16	Sterne	Laurence	1713	1768
+10	Kritzer	Naomi	1973	\N
+11	Bujold	Lois McMaster	1949	\N
+12	Melville	Herman	1819	1891
+13	Sterne	Laurence	1713	1768
+14	Osman	Richard	1970	\N
+15	Brontë	Ann	1820	1849
+16	Brontë	Emily	1818	1848
 \.
 
 
@@ -87,33 +91,33 @@ COPY public.books (id, title, publication_year) FROM stdin;
 1	And Then There Were None	1939
 2	Beloved	1987
 3	Blackout	2010
-4	Elmer Gantry	1927
-5	Emma	1815
-6	Good Omens	1990
+4	Character Limit	2024
+5	Elmer Gantry	1927
+6	Emma	1815
 7	Jane Eyre	1847
 8	Leave it to Psmith	1923
 9	Love in the Time of Cholera	1985
-10	Main Street	1920
-11	Midnight's Children	1981
+10	Catfishing on Catnet	2019
+11	Main Street	1920
 12	Mirror Dance	1994
 13	Moby Dick	1851
 14	Murder on the Orient Express	1934
-15	Neverwhere	1996
-16	Omoo	1847
-17	One Hundred Years of Solitude	1967
-18	Pride and Prejudice	1813
-19	Right Ho, Jeeves	1934
+15	Omoo	1847
+16	One Hundred Years of Solitude	1967
+17	Pride and Prejudice	1813
+18	Right Ho, Jeeves	1934
+19	Liberty's Daughter	2023
 20	Sense and Sensibility	1813
 21	Shards of Honor	1986
 22	Sula	1973
 23	The Code of the Woosters	1938
-24	The Satanic Verses	1988
-25	The Tenant of Wildfell Hall	1848
-26	Thief of Time	1996
+24	The Life and Opinions of Tristram Shandy, Gentleman	1759
+25	The Thursday Murder Club	2020
+26	The Tenant of Wildfell Hall	1848
 27	To Say Nothing of the Dog	1997
 28	Villette	1853
-29	Wuthering Heights	1847
-30	The Life and Opinions of Tristram Shandy, Gentleman	1759
+29	The Man Who Died Twice	2021
+30	Wuthering Heights	1847
 \.
 
 
@@ -126,33 +130,33 @@ COPY public.books_authors (book_id, author_id) FROM stdin;
 1	1
 2	2
 3	0
-4	10
-5	4
-6	5
+4	3
+4	4
+5	5
 6	6
 7	7
 8	8
 9	9
 10	10
-11	11
-12	12
-13	13
+11	5
+12	11
+13	12
 14	1
-15	5
-16	13
-17	9
-18	4
-19	8
-20	4
-21	12
+15	12
+16	9
+17	6
+18	8
+19	10
+20	6
+21	11
 22	2
 23	8
-24	11
+24	13
 25	14
-26	6
+26	15
 27	0
 28	7
-29	15
+29	14
 30	16
 \.
 
